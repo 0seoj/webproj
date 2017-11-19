2f856bcb778c999ac07086a73be8a0c274cc5702
Jung Yeong Seo <pp1312pp@naver.com>
2017-11-19 16:58:54 +0900

    sign up

--- routes/index.js
+++ routes/index.js
@@ -9,5 +9,18 @@ router.get('/', function(req, res, next) {
 router.get('/signin', function(req, res, next) {
   res.render('signin');
 });
-
+router.post('/signin', function(req, res, next) {
+  User.findOne({email: req.body.email}, function(err, user) {
+    if (err) {
+      res.render('error', {message: "Error", error: err});
+    } else if (!user || user.password !== req.body.password) {
+      req.flash('danger', 'Invalid username or password.');
+      res.redirect('back');
+    } else {
+      req.session.user = user;
+      req.flash('success', 'Welcome!');
+      res.redirect('/');
+    }
+  });
+});
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
+router.get('/new', (req, res, next) => {
+  res.render('users/new', {messages: req.flash()});
+});
 module.exports = router;
